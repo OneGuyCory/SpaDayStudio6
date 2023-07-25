903455802218c7630da25f0b5b09e26a75820ace
Cory <corydeuschle92@gmail.com>
2023-07-24 22:16:20 -0500

    Studio finished

--- SpaDay6/Controllers/SpaController.cs
+++ SpaDay6/Controllers/SpaController.cs
@@ -56,6 +56,12 @@ namespace SpaDay6.Controllers
                     appropriateFacials.Add(facials[i]);
                 }
             }
+
+            ViewBag.name = name;
+            ViewBag.skintype = skintype;
+            ViewBag.appropiatefacials = appropriateFacials;
+            ViewBag.manipedi = manipedi;
+
             return View();
         }
     }
